7a960d623675ce167ffb64eefcb9e5bab49d18c2
Mike Bostock <mbostock@gmail.com>
2021-06-04 11:01:54 -0700

    update dependencies

--- test/path-test.js
+++ test/path-test.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import assertPathEqual from "./asserts.js";
+import {assertPathEqual} from "./asserts.js";
 import {path} from "../src/index.js";
 
 it("path is an instanceof path", () => {
